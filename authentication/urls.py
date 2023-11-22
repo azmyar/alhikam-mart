952c94db6b8fc50cff6b2096b92a2e6a8e8d9adf
azmyar <azmy.arya.rizaldi@gmail.com>
2023-11-22 10:00:42 +0700

    Tugas 9 Done

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from authentication.views import login, register, logout
+from authentication.views import login, logout
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('register/', register, name='register'),
     path('logout/', logout, name='logout'),
 ]
